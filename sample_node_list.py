594f73d08ad91b41ea860e669f4990d482378623
crypto-ali <35846355+crypto-ali@users.noreply.github.com>
2021-04-09 15:53:52 -0400

    Add oxen.observer to remote_node_list.

--- sample_node_list.py
+++ sample_node_list.py
@@ -2,15 +2,16 @@
 snode_list = [
     'Loki Service Node 1 PubKey',
     'Loki Service Node 2 PubKey',
-    'Loki Service Node 3 PubKey'
+    'Loki Service Node 3 PubKey',
 ]
 
 """List of Loki remote nodes. Use these default nodes, 
 change them, or add more."""
 remote_node_list = [
-    'http://lokiblocks.com:22023/json_rpc',
+    'http://oxen.observer:22023/json_rpc',
     'http://public.loki.foundation:22023/json_rpc',
     'http://nodes.hashvault.pro:22023/json_rpc',
     'http://imaginary.stream:22023/json_rpc',
-    'http://node.loki-pool.com:18081/json_rpc'
+    'http://node.loki-pool.com:18081/json_rpc',
+    'http://lokiblocks.com:22023/json_rpc',
 ]
