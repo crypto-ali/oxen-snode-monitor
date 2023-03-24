3390694fe04ccbee1ee27a9a4aea99f563cc420d
crypto-ali <35846355+crypto-ali@users.noreply.github.com>
2023-03-23 20:36:21 -0400

    Add new environment variable to manage logger file handler
    Refactor status_logger file

--- status_logger.py
+++ status_logger.py
@@ -6,29 +6,37 @@ load_dotenv()
 
 # Variables
 LOG_LEVEL = os.getenv('LOG_LEVEL')
+RUNNING = os.getenv('RUNNING')
 
 logger = logging.getLogger(__name__)
 logger_level = logging.getLevelName(LOG_LEVEL)
 logger.setLevel(logger_level)
 
 
-# Handlers
+# Handler
 c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler('status.log')
-c_handler.setLevel(logging.INFO)
-f_handler.setLevel(logging.INFO)
+c_handler.setLevel(logging.DEBUG)
+
 
 # Create formatters and add it to handlers
 c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                             datefmt='%a %b %d %Y - %I:%M:%S %p')
 c_handler.setFormatter(c_format)
-f_handler.setFormatter(f_format)
+
 
 # Add handlers to the logger
 logger.addHandler(c_handler)
-logger.addHandler(f_handler)
+
+if RUNNING == 'SCREEN':
+    # Initialize logging file handler and file formatter
+    f_handler = logging.FileHandler('status.log')
+    f_handler.setLevel(logging.DEBUG)
+    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                 datefmt='%a %b %d %Y - %I:%M:%S %p')
+    f_handler.setFormatter(f_format)
+    logger.addHandler(f_handler)
 
 if __name__ == "__main__":
+    logger.debug("This is an DEBUG log.")
     logger.info("This is an INFO log.")
     logger.warning("This is a WARNING log.")
+    logger.error("This is an ERROR log.")
