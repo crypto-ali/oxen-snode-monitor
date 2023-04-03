e4d4a35e763a34835a321ea029bdfe22681c24be
crypto-ali <35846355+crypto-ali@users.noreply.github.com>
2023-04-03 12:49:02 -0400

    Update list of remote nodes

--- sample_node_list.py
+++ sample_node_list.py
@@ -10,9 +10,8 @@ change them, or add more."""
 remote_node_list = [
     'http://oxen.observer:22023/json_rpc',
     'http://public.loki.foundation:22023/json_rpc',
-    'http://nodes.hashvault.pro:22023/json_rpc',
     'http://imaginary.stream:22023/json_rpc',
-    'http://node.loki-pool.com:18081/json_rpc',
     'http://lokiblocks.com:22023/json_rpc',
     'http://explorer.oxen.aussie-pools.com:18081/json_rpc',
+    'http://oxen.caliban.org:22023/json_rpc',
 ]
