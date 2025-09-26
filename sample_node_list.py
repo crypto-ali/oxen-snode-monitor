65c06b18d1fcb97a23e215d15657e69903be5960
crypto-ali <35846355+crypto-ali@users.noreply.github.com>
2025-09-26 16:40:00 -0400

    Update remote_node-list

--- sample_node_list.py
+++ sample_node_list.py
@@ -5,13 +5,11 @@ snode_list = [
     'Loki Service Node 3 PubKey',
 ]
 
-"""List of Loki remote nodes. Use these default nodes, 
+"""List of Session remote nodes. Use these default nodes, 
 change them, or add more."""
 remote_node_list = [
     'http://oxen.observer:22023/json_rpc',
     'http://public.loki.foundation:22023/json_rpc',
     'http://imaginary.stream:22023/json_rpc',
-    'http://lokiblocks.com:22023/json_rpc',
-    'http://explorer.oxen.aussie-pools.com:18081/json_rpc',
-    'http://oxen.caliban.org:22023/json_rpc',
+    'http://public-eu.optf.ngo:22023/json_rpc',
 ]
